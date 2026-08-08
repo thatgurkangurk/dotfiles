58eebe1d31285efa6499c9d2d2983e518fc718bc
Gurkan <hello@gurkz.me>
2026-08-08 13:45:27 +0200

    wsl + bin fixes
    Signed-off-by: Gurkan <hello@gurkz.me>

--- dot_config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set -g fish_greeting
-set -g fish_color_autosuggestion 5f87af
-
-mise activate fish | source
-starship init fish | source
