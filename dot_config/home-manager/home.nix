4c8f00d4b6958c354af388275dc927f6da04d332
Gurkan <hello@gurkz.me>
2026-08-18 22:09:24 +0200

    nix topgrade + homemanager
    Signed-off-by: Gurkan <hello@gurkz.me>

---
+++ dot_config/home-manager/home.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  home.username = "gurkan";
+  home.homeDirectory = "/home/gurkan";
+
+  home.stateVersion = "26.05";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # nix language server stuff
+    nil
+    nixd
+
+    # cli utils
+    chezmoi
+    starship
+    mise
+    ripgrep
+    fd
+    eza
+    bat
+    fzf
+    topgrade
+
+    # gui apps
+    zed-editor
+  ];
+}
